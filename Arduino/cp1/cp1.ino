37b1f6d7e1bf0748fd31731035f919fc9336269a
sunfu-chou <sunfu.chou@gmail.com>
2022-10-17 20:58:50 +0800

    Feat: Add arduino libraries

---
+++ Arduino/cp1/cp1.ino
@@ -0,0 +1,29 @@
+#include <ros.h>
+#include <std_msgs/Int64.h>
+
+void num_cb(const std_msgs::Int64 &msg);
+
+ros::NodeHandle nh;
+std_msgs::Int64 num;
+
+ros::Publisher num_pub("num/arduino", &num);
+ros::Subscriber<std_msgs::Int64> num_sub("num/raw", &num_cb);
+
+void num_cb(const std_msgs::Int64& msg){
+  num = msg;
+  num.data *= 2;
+  num_pub.publish(&num);
+}
+
+void setup()
+{
+  nh.initNode();
+  nh.advertise(num_pub);
+  nh.subscribe(num_sub);
+}
+01
+
+void loop()
+{
+  nh.spinOnce();
+}
